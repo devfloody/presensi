80704c825b763729c82bb188c6d12efbfcf76886
FloodyXD <fahmiismail125@gmail.com>
2022-05-10 10:45:18 +0700

    add jadwal berhasil

--- lib/app/modules/add_jadwal/controllers/add_jadwal_controller.dart
+++ lib/app/modules/add_jadwal/controllers/add_jadwal_controller.dart
@@ -1,10 +1,46 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class AddJadwalController extends GetxController {
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  TextEditingController praktCtrl = TextEditingController();
+  TextEditingController kodeCtrl = TextEditingController();
+  TextEditingController kelasCtrl = TextEditingController();
+  TextEditingController dosenCtrl = TextEditingController();
+  TextEditingController ruangCtrl = TextEditingController();
+  TextEditingController jmlmhsCtrl = TextEditingController();
 
-  Stream<QuerySnapshot<Map<String, dynamic>>> streamJadwal() async* {
-    yield* firestore.collection('praktikum').orderBy('kode').snapshots();
+  RxBool isLoading = false.obs;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  Future<void> addJadwal() async {
+    if (praktCtrl.text.isNotEmpty &&
+        kodeCtrl.text.isNotEmpty &&
+        kelasCtrl.text.isNotEmpty &&
+        dosenCtrl.text.isNotEmpty &&
+        ruangCtrl.text.isNotEmpty &&
+        jmlmhsCtrl.text.isNotEmpty) {
+      isLoading.value = true;
+      String uid = await auth.currentUser!.uid;
+      String jadwalId = '${kodeCtrl.text}-${kelasCtrl.text}';
+      try {
+        isLoading.value = false;
+        await db.collection('pengguna').doc(uid).collection('jadwal').doc(jadwalId).set({
+          'praktikum': praktCtrl.text,
+          'kode': kodeCtrl.text,
+          'kelas': kelasCtrl.text,
+          'dosen': dosenCtrl.text,
+          'ruang': ruangCtrl.text,
+          'jml_mhs': int.parse(jmlmhsCtrl.text),
+        });
+        Get.back();
+        Get.snackbar('Berhasil', 'Jadwal berhasil ditambahkan');
+      } catch (e) {
+        isLoading.value = false;
+        Get.snackbar('Terjadi Kesalahan', 'Gagal menambahkan jadwal');
+      }
+    }
   }
 }
--- lib/app/modules/add_jadwal/views/add_jadwal_view.dart
+++ lib/app/modules/add_jadwal/views/add_jadwal_view.dart
@@ -1,9 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:presensi/app/config/theme.dart';
 
+import '../../../config/theme.dart';
 import '../controllers/add_jadwal_controller.dart';
 
 class AddJadwalView extends GetView<AddJadwalController> {
@@ -14,125 +13,121 @@ class AddJadwalView extends GetView<AddJadwalController> {
         title: Text('Tambah Jadwal'),
         centerTitle: true,
       ),
-      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: controller.streamJadwal(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasData) {
-            final data = snapshot.requireData;
-            return ListView.builder(
-              padding: EdgeInsets.all(16),
-              itemCount: data.docs.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.only(bottom: 12),
-                  padding: EdgeInsets.all(16),
-                  height: 160,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(color: CustomColor.lightGrey),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Praktikum\n${data.docs[index].data()['nama']}',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600,
-                                  color: CustomColor.black,
-                                ),
-                              ),
-                              SizedBox(height: 4),
-                              Text('Kode : ${data.docs[index].data()['kode']}'),
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Dosen : ${data.docs[index].data()['dosen']}',
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w500,
-                                  color: CustomColor.black,
-                                ),
-                              ),
-                              Text(
-                                'Ruang : ${data.docs[index].data()['ruang']}',
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w400,
-                                  color: CustomColor.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: CustomColor.primary,
-                            ),
-                            child: Center(
-                              child: Text(
-                                data.docs[index].data()['kelas'],
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 28,
-                                  color: CustomColor.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: CustomColor.secondary,
-                            ),
-                            child: Center(
-                              child: IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.add,
-                                  size: 32,
-                                  color: CustomColor.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          }
-          return Center(
-            child: Text('Tidak dapat memuat data'),
-          );
-        },
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        physics: BouncingScrollPhysics(),
+        children: [
+          TextField(
+            controller: controller.praktCtrl,
+            decoration: InputDecoration(
+              hintText: 'Nama Praktikum',
+              contentPadding: EdgeInsets.all(16),
+              hintStyle: Theme.of(context).textTheme.headline5!.copyWith(color: CustomColor.grey),
+              fillColor: CustomColor.lightGrey,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(4),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: controller.kodeCtrl,
+            decoration: InputDecoration(
+              hintText: 'Kode Praktikum',
+              contentPadding: EdgeInsets.all(16),
+              hintStyle: Theme.of(context).textTheme.headline5!.copyWith(color: CustomColor.grey),
+              fillColor: CustomColor.lightGrey,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(4),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: controller.kelasCtrl,
+            decoration: InputDecoration(
+              hintText: 'Kelas',
+              contentPadding: EdgeInsets.all(16),
+              hintStyle: Theme.of(context).textTheme.headline5!.copyWith(color: CustomColor.grey),
+              fillColor: CustomColor.lightGrey,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(4),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: controller.dosenCtrl,
+            decoration: InputDecoration(
+              hintText: 'Dosen Pengampu',
+              contentPadding: EdgeInsets.all(16),
+              hintStyle: Theme.of(context).textTheme.headline5!.copyWith(color: CustomColor.grey),
+              fillColor: CustomColor.lightGrey,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(4),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: controller.ruangCtrl,
+            decoration: InputDecoration(
+              hintText: 'Ruang Praktikum',
+              contentPadding: EdgeInsets.all(16),
+              hintStyle: Theme.of(context).textTheme.headline5!.copyWith(color: CustomColor.grey),
+              fillColor: CustomColor.lightGrey,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(4),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: controller.jmlmhsCtrl,
+            decoration: InputDecoration(
+              hintText: 'Jumlah Mahasiswa',
+              contentPadding: EdgeInsets.all(16),
+              hintStyle: Theme.of(context).textTheme.headline5!.copyWith(color: CustomColor.grey),
+              fillColor: CustomColor.lightGrey,
+              filled: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(4),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          SizedBox(height: 32),
+          Obx(
+            () => SizedBox(
+              height: 50,
+              width: double.infinity,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: CustomColor.primary,
+                ),
+                onPressed: () async {
+                  if (controller.isLoading.isFalse) {
+                    await controller.addJadwal();
+                  }
+                },
+                child: Text(
+                  controller.isLoading.isFalse ? 'Tambah Jadwal Praktikum' : 'Loading...',
+                  style: Theme.of(context).textTheme.button?.copyWith(color: CustomColor.white),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
